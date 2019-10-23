b2e92a34cd4645916b7b06166df0973fcc537e2e
test <test@qq.com>
2019-10-23 17:26:49 +0800

    source code push

---
+++ config/plugin.js
@@ -0,0 +1,45 @@
+'use strict';
+
+/** @type Egg.EggPlugin */
+module.exports = {
+  // had enabled by egg
+  // static: {
+  //   enable: true,
+  // }
+  validatePlus: {
+    enable: true,
+    package: 'egg-validate-plus',
+  },
+  
+  utils: {
+    enable: true,
+    package: '@taccisum/egg-utils',
+  },
+      
+  eureka: {
+    enable: true,
+    package: '@taccisum/egg-eureka',
+  },
+      
+  swaggerdoc: {
+    enable: true,
+    package: 'egg-swagger-doc',
+  },
+      
+  tx: {
+    enable: true,
+    package: 'egg-tx',
+  },
+      
+  mongoose: {
+    enable: true,
+    package: 'egg-mongoose',
+  },
+      
+  apollo: {
+    enable: true,
+    package: '@taccisum/egg-apollo',
+  },
+      
+
+};
